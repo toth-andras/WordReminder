382b2b9daf3dc97caeb509c842a97adeff51a0d4
ТОТ АНДРАШ <andras.toth@mail.ru>
2021-07-17 15:36:36 +0300

    Added basic Ui for Home page.

---
+++ WordReminder_PC/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordReminder_PC.Core;
+
+namespace WordReminder_PC.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        private object currentView;
+        public HomeViewModel HomeVM { get; set; }
+
+        public object CurrentView
+        {
+            get { return currentView; }
+            set { currentView = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
