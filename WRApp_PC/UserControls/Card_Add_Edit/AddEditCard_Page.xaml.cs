31d3e38848e80860c82cda64ceb48fe8a8871a17
ТОТ АНДРАШ <andras.toth@mail.ru>
2021-08-14 01:22:44 +0300

    Started reorganizing page system. Started adding Add/Edit Card page.

---
+++ WRApp_PC/UserControls/Card_Add_Edit/AddEditCard_Page.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using WRApp_PC.WRLibrary;
+
+namespace WRApp_PC.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AddEditCard_Page.xaml
+    /// </summary>
+    
+    // Тип страницы.
+    enum PageType { AddCard, EditCard }
+
+    public partial class AddEditCard_Page : UserControl
+    {
+        private Card card;
+        private PageType type;
+
+        /// <summary>
+        /// Вызывается, когда странница произвела все необходимые действия и может быть закрыта.
+        /// </summary>
+        public Action OnFinished;
+
+        public AddEditCard_Page()
+        {
+            InitializeComponent();
+
+            // Если карточка не передана - значит, нужен режим создания.
+            type = PageType.AddCard;
+
+            SetUI();
+        }
+        public AddEditCard_Page(Card card)
+        {
+            InitializeComponent();
+
+            this.card = card;
+            type = PageType.EditCard;
+
+            SetUI();
+        }
+
+        // Формирует интрефейс страницы, основываясь на типе страницы.
+        private void SetUI()
+        {
+            switch (type)
+            {
+                case PageType.AddCard:
+                    AddCardType();
+                    break;
+
+                case PageType.EditCard:
+                    EditCardType();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        // Формирует интерфейс страницы для типа страницы "Добавить карточку".
+        private void AddCardType()
+        {
+            PageTitleLabel.Content = "Добавить карточку";
+        }
+
+        // Формирует интерфейс страницы для типа страницы "Редактировать карточку".
+        private void EditCardType()
+        {
+            PageTitleLabel.Content = "Редактировать карточку";
+        }
+    }
+}
